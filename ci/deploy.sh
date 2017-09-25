6d22d0f0411ae7529161c8d9278c5fcf655dc20f
Rubén Baztán <ruazulgrana10@gmail.com>
2017-09-25 12:03:50 +0200

    [#39] Change of the deploy tag to 8 characters

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -10,7 +10,9 @@ if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
     exit 0
 fi
 
-IMAGE_NAME="akvo/keycloak-ha-mysql:${TRAVIS_COMMIT}"
+TAG=$(echo -n "${TRAVIS_COMMIT}" | cut -c-8)
+
+IMAGE_NAME="akvo/keycloak-ha-mysql:${TAG}"
 
 # Pushing images
 docker login -u="${DOCKERHUB_USERNAME}" -p="${DOCKERHUB_PASSWORD}"
