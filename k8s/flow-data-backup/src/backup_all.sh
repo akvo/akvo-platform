d4cd77b697117280a37974a93c38341c35c1b386
Daniel Lebrero Berna <dlebrero@gmail.com>
2019-09-10 13:34:26 +0200

    [#172] Enable backup for all instances

--- k8s/flow-data-backup/src/backup_all.sh
+++ k8s/flow-data-backup/src/backup_all.sh
@@ -9,8 +9,5 @@ tar xzvf flow-config.tar.gz --strip-components=1 --directory=${FLOW_CONFIG_DIREC
 
 for i in `find ${FLOW_CONFIG_DIRECTORY} | grep p12 | cut -f 2 -d/ | sort`; do
     echo "Starting backup for $i"
-#    ./backup.sh ${i} ${FLOW_CONFIG_DIRECTORY}/${i} || echo "Backup failed for $i"
+    ./backup.sh ${i} ${FLOW_CONFIG_DIRECTORY}/${i} || echo "Backup failed for $i"
 done
-
-i=akvoflow-dev2
-./backup.sh ${i} ${FLOW_CONFIG_DIRECTORY}/${i} || echo "Backup failed for $i"
