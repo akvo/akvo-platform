fbd8efa90224db99fb942a110e25b6dfa9561ee6
Daniel Lebrero Berna <dlebrero@gmail.com>
2019-11-12 13:46:22 +0100

    [#201] kubectl apply test and prod ingress on CI

---
+++ k8s/ingress/ci/deploy.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -eu
+
+function log {
+   echo "$(date +"%T") - INFO - $*"
+}
+
+export PROJECT_NAME=akvo-lumen
+
+if [[ "${TRAVIS_BRANCH}" != "master" ]]; then
+    exit 0
+fi
+
+if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
+    exit 0
+fi
+
+log Making sure gcloud and kubectl are installed and up to date
+gcloud components install kubectl
+gcloud components update
+gcloud version
+which gcloud kubectl
+
+log Authentication with gcloud and kubectl
+gcloud auth activate-service-account --key-file "${GCLOUD_ACCOUNT_FILE}"
+gcloud config set project akvo-lumen
+gcloud config set container/cluster europe-west1-d
+gcloud config set compute/zone europe-west1-d
+gcloud config set container/use_client_certificate True
+
+log Environment is test
+gcloud container clusters get-credentials test
+kubectl apply -f test.yaml
+
+log Environment is production
+gcloud container clusters get-credentials production
+kubectl apply -f prod.yaml
