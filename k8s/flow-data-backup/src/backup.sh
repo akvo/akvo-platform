f0aab86dc225e403d16e0ceedf8b7cb1eb75f597
Iván Perdomo <ivan@perdomo.me>
2019-12-19 17:57:35 +0100

    [#205] Code clean up - No functional changes
    * Use "${variable}" when possible
    * Use --format=json and `jq` when parsing responses from `gcloud`
    * Simplify shell calls when using multiple pipes `|`
    * We only create buckets on us-central1 or eu-west1 locations

--- k8s/flow-data-backup/src/backup.sh
+++ k8s/flow-data-backup/src/backup.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
 
+set -u
+
 #eval "$(sentry-cli bash-hook)"
 
 function log {
    echo "$(date +"%T") - INFO - $*"
 }
 
-instance=$1
+instance="${1}"
 ## We are adding the instance as a env var so that sentry-cli adds it to the failure message, so that we know which instance failed.
-export FLOW_INSTANCE=${instance}
-instance_config_directory=$2
-bucket=gs://akvobackup-${instance}
-bucket_path=${bucket}/$(date "+%FT%T")
-p12_file=${instance_config_directory}/${instance}.p12
+export FLOW_INSTANCE="${instance}"
+instance_config_directory="${2}"
+bucket="gs://akvobackup-${instance}"
+bucket_path="${bucket}/$(date '+%FT%T')"
+p12_file="${instance_config_directory}/${instance}.p12"
 
 export CLOUDSDK_PYTHON_SITEPACKAGES=1
 
-account=$(cat ${instance_config_directory}/appengine-web.xml | grep serviceAccountId | grep -o "value=.*" | cut -f 2 -d\")
+account=$(sed -n 's:.*serviceAccountId.*value="\(.*\)".*:\1:p' "${instance_config_directory}/appengine-web.xml")
 
-log Account for ${instance} is --${account}--
-gcloud auth activate-service-account ${account} --key-file=${p12_file}  --project ${instance}
+log Account for "${instance}" is "--${account}--"
+gcloud auth activate-service-account "${account}" --key-file="${p12_file}" --project "${instance}"
 
 if ! gcloud services list | grep appengine; then
     log "Enabling API appengine.googleapis.com. This sometimes takes a bit of time which causes the bucket creation to not work"
@@ -36,25 +38,25 @@ if ! gcloud services list | grep storage-component; then
     gcloud services enable storage-component.googleapis.com
 fi
 
-if ! gsutil ls ${bucket}; then
-    project_location=$(gcloud app describe | grep locationId | cut -f2 -d\ )
-    region=$(echo ${project_location} | cut -b1-2)
+if ! gsutil ls "${bucket}"; then
+    project_location=$(gcloud app describe --format=json | jq -M -r .locationId)
+    region="${project_location}1" # [us-central1 | eu-west1] - See: https://cloud.google.com/storage/docs/locations
     if [[ -z "${region}" ]]; then
-        log "Cannot determine the region for the bucket given the location --$project_location--"
+        log "Cannot determine the region for the bucket given the location --${project_location}--"
         exit 1
     fi
     log "Creating bucket ${bucket} in region ${region} (from location ${project_location})"
-    gsutil mb -l ${region} -c coldline --retention 90d ${bucket}
+    gsutil mb -l "${region}" -c coldline --retention 90d "${bucket}"
 fi
 
-python3 convert.py ${instance} ${p12_file} ${account} service-account.json
+python3 convert.py "${instance}" "${p12_file}" "${account}" service-account.json
 export GOOGLE_APPLICATION_CREDENTIALS=service-account.json
 
-if gcloud app describe | grep servingStatus | grep SERVING; then
-  kinds=$(python3 kinds.py ${instance})
-  echo $kinds
-  log backing up ${instance} to ${bucket_path}, kinds: ${kinds}
-  gcloud datastore export --namespaces="(default)" --kinds=${kinds} ${bucket_path}
+if [[ $(gcloud app describe --format=json | jq -M -r.servingStatus) == "SERVING" ]]; then
+  kinds=$(python3 kinds.py "${instance}")
+  echo "${kinds}"
+  log backing up "${instance}" to "${bucket_path}, kinds: ${kinds}"
+  gcloud datastore export --namespaces="(default)" --kinds="${kinds}" "${bucket_path}"
 else
-  log Instance ${instance} not in serviring state. Not backing up instance ${instance}
+  log Instance "${instance}" not in serviring state. Not backing up instance "${instance}"
 fi
--- k8s/flow-data-backup/src/backup_all.sh
+++ k8s/flow-data-backup/src/backup_all.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
+set -u
+
 eval "$(sentry-cli bash-hook)"
 
 FLOW_CONFIG_DIRECTORY=flow-config
-mkdir ${FLOW_CONFIG_DIRECTORY}
-curl --header "Authorization: token ${GITHUB_AUTH_TOKEN}" -L https://api.github.com/repos/akvo/akvo-flow-server-config/tarball/master > flow-config.tar.gz
-tar xzvf flow-config.tar.gz --strip-components=1 --directory=${FLOW_CONFIG_DIRECTORY}
+mkdir "${FLOW_CONFIG_DIRECTORY}"
+curl --header "Authorization: token ${GITHUB_AUTH_TOKEN}" \
+     --location "https://api.github.com/repos/akvo/akvo-flow-server-config/tarball/master" > flow-config.tar.gz
+tar xzvf flow-config.tar.gz --strip-components=1 --directory="${FLOW_CONFIG_DIRECTORY}"
 
-for i in `find ${FLOW_CONFIG_DIRECTORY} | grep p12 | cut -f 2 -d/ | sort`; do
-    echo "Starting backup for $i"
-    ./backup.sh ${i} ${FLOW_CONFIG_DIRECTORY}/${i} || echo "Backup failed for $i"
+for i in $(find "${FLOW_CONFIG_DIRECTORY}" -type f -name '*.p12' | cut -f 2 -d/ | sort); do
+    echo "Starting backup for ${i}"
+    ./backup.sh "${i}" "${FLOW_CONFIG_DIRECTORY}/${i}" || echo "Backup failed for ${i}"
 done
