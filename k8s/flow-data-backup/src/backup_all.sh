833f18fe261c9ff3ae83259cf441a9b8967c7f80
Iván Perdomo <ivan@perdomo.me>
2019-12-19 18:23:05 +0100

    [#205] Skip instances that have `.skip-backup` flag

--- k8s/flow-data-backup/src/backup_all.sh
+++ k8s/flow-data-backup/src/backup_all.sh
@@ -11,6 +11,12 @@ curl --header "Authorization: token ${GITHUB_AUTH_TOKEN}" \
 tar xzvf flow-config.tar.gz --strip-components=1 --directory="${FLOW_CONFIG_DIRECTORY}"
 
 for i in $(find "${FLOW_CONFIG_DIRECTORY}" -type f -name '*.p12' | cut -f 2 -d/ | sort); do
+
+    if [[ -f "${FLOW_CONFIG_DIRECTORY}/${i}/.skip-backup" ]]; then
+	echo "Skipping backup for ${i}"
+	continue
+    fi
+
     echo "Starting backup for ${i}"
     ./backup.sh "${i}" "${FLOW_CONFIG_DIRECTORY}/${i}" || echo "Backup failed for ${i}"
 done
