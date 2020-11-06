68a53ba6a7768bf63ce5401a7590ff90c0de711c
Puneeth Chaganti <punchagan@muse-amuse.in>
2020-11-06 19:08:00 +0530

    Work in /tmp/akvo/github-pull-reminders
    Remove multiple references to /tmp/akvo/github-pull-reminders and work
    out of the directory, when running the script.

--- k8s/zulip-pull-reminders/pull-reminders.sh
+++ k8s/zulip-pull-reminders/pull-reminders.sh
@@ -2,7 +2,8 @@
 
 set -euo pipefail
 
-# rm -rf /tmp/akvo/github-pull-reminders
+DATA_DIR=/tmp/akvo/github-pull-reminders
+
 mkdir -p /tmp/akvo/github-pull-reminders
 
 github_fetch() {
@@ -17,20 +18,20 @@ github_fetch() {
 }
 
 download_repos() {
-    github_fetch https://api.github.com/orgs/akvo/repos?per_page=100 > "/tmp/akvo/github-pull-reminders/repos.json"
+    github_fetch https://api.github.com/orgs/akvo/repos?per_page=100 > "repos.json"
 }
 
 create_repos_list() {
-    repos="$(jq -r '.[].name' /tmp/akvo/github-pull-reminders/repos.json)"
-    echo "$repos" > "/tmp/akvo/github-pull-reminders/repos.txt"
+    repos="$(jq -r '.[].name' repos.json)"
+    echo "$repos" > "repos.txt"
 }
 
 download_open_pulls_for_repo() {
-    github_fetch "https://api.github.com/repos/akvo/$1/pulls?state=open" > "/tmp/akvo/github-pull-reminders/pulls-$1.json"
+    github_fetch "https://api.github.com/repos/akvo/$1/pulls?state=open" > "pulls-$1.json"
 }
 
 download_open_pulls_for_all_repos() {
-    for repo in $(cat /tmp/akvo/github-pull-reminders/repos.txt)
+    for repo in $(cat repos.txt)
     do
         download_open_pulls_for_repo "$repo"
         #list_open_pull_requests "$var"
@@ -38,7 +39,7 @@ download_open_pulls_for_all_repos() {
 }
 
 list_open_pulls_for_repo() {
-    open_prs="$(jq -r '.[] | [.title, (.requested_reviewers | map(.login) | join(", ")), .html_url]  | join(" ")' /tmp/akvo/github-pull-reminders/pulls-$1.json)"
+    open_prs="$(jq -r '.[] | [.title, (.requested_reviewers | map(.login) | join(", ")), .html_url]  | join(" ")' pulls-$1.json)"
     if [ ! -z "$open_prs" ]
     then
        printf "\n\n### $1\n"
@@ -47,7 +48,7 @@ list_open_pulls_for_repo() {
 }
 
 list_open_pulls_for_all_repos() {
-    for repo  in $(cat /tmp/akvo/github-pull-reminders/repos.txt)
+    for repo  in $(cat repos.txt)
     do
         list_open_pulls_for_repo "$repo"
     done
@@ -66,11 +67,13 @@ github_username_to_zulip_name() {
     curl --silent --show-error --fail \
         --get https://akvo.zulipchat.com/api/v1/users \
         --user "${ZULIP_TOKEN}" \
-        --data 'include_custom_profile_fields=true' | jq -Mr '.members[]|.profile_data."1925".value+":"+.full_name' > /tmp/akvo/github-pull-reminders/github-to-zulip.txt
+        --data 'include_custom_profile_fields=true' | jq -Mr '.members[]|.profile_data."1925".value+":"+.full_name' > github-to-zulip.txt
 }
 
+pushd ${DATA_DIR}
 download_repos
 create_repos_list
 download_open_pulls_for_all_repos
 
 post_to_zulip "$(list_open_pulls_for_all_repos)"
+popd
